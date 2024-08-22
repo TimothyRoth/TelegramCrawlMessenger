60412c29dccc633c7dfc180bbd6dfebe811c28e5
TimothyRoth <111071287+TimothyRoth@users.noreply.github.com>
2024-08-22 14:36:01 +0200

    Update cron.php
    moving valid token to the constants

--- cron.php
+++ cron.php
@@ -9,7 +9,6 @@ require_once __DIR__ . '/config.php';
 require_once __APP_ROOT_DIRECTORY__ . '/app/index.php';
 
 $token = $_GET['token'] ?? '';
-$valid_token = '<insert token here>';
 
 if ($token !== $valid_token) {
     http_response_code(403);
